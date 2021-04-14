c24190ae5913ea32215bfb8cfa19bdb72960e251
Zapra Gartiast <zapragartiast@gmail.com>
2021-04-14 22:18:59 +0700

    Adding gunicorn for the server runtime

--- main.py
+++ main.py
@@ -2,9 +2,53 @@ import os
 import unittest
 import coverage
 
-from flask_script import Manager
+from flask_script import Manager, Command, Option
 from flask_migrate import Migrate, MigrateCommand
 from src.app import app, db
+
+
+class GunicornServer(Command):
+    """Run the app with gunicorn server"""
+    def __init__(self, host='0.0.0.0', port=5000, workers=4):
+        self.host = host
+        self.port = port
+        self.workers = workers
+
+    def get_options(self):
+        return (
+            Option('-H', '--host',
+                   dest='host',
+                   default=self.host),
+            Option('-p', '--port',
+                   dest='port',
+                   type=int,
+                   default=self.port),
+            Option('-w', '--workers',
+                   dest='workers',
+                   type=int,
+                   default=self.workers)
+        )
+
+    def __call__(self, app, host, port, workers):
+        from gunicorn import version_info
+        if version_info < (0, 9, 0):
+            from gunicorn.arbiter import Arbiter
+            from gunicorn.config import Config
+            arbiter = Arbiter(Config({'bind': '%s:%d' % (host, int(port)), 'workers': workers}), app)
+            arbiter.run()
+        else:
+            from gunicorn.app.base import Application
+
+            class FlaskApplication(Application):
+                def init(self, parser, opts, args):
+                    return {
+                        'bind': '{0}:{1}'.format(host, port)
+                    }
+
+                def load(self):
+                    return app
+            FlaskApplication().run()
+
 
 APP_COVERAGE = coverage.coverage(
     branch=True,
@@ -23,6 +67,7 @@ migrate = Migrate(app, db, compare_type=True)
 
 # do migration
 manager.add_command('migrasi', MigrateCommand)
+manager.add_command('gunicorn', GunicornServer())
 
 
 @manager.command
